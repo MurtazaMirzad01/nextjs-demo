c2f3b3b777e129ad2939324b3eef1cd7965dc1fa
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-06-24 21:43:47 +0430

    Implement file colocation & private folder

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+function BarChart() {
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard(){
+    return (
+        <html lang="en">
+            <body>
+                <h1 className="text-4xl font-bold">Dashboard</h1>
+            </body>
+        </html>
+    )
+};
