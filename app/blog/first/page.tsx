845b6f1eded3780e104709d3f07036faaa913bed
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-06-24 18:32:58 +0430

    Implement nested routes

---
+++ app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+export default function FirstBlogPost() {
+    return (
+        <html lang="en">
+            <body>
+                <h1 className="text-4xl font-bold"> First Blog Post!</h1>
+            </body>
+        </html>
+    );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <html lang="en">
+            <body>
+                <h1 className="text-4xl font-bold">Welcome to the Blog Page!</h1>
+            </body>
+        </html>
+    );
+}
