1426f10d7d9170d3dcb8da4d147fe506cac96884
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-06-25 13:31:16 +0430

    Create header & footer layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-    return (
-        <html lang="en">
-            <body>
-                <h1 className="text-4xl font-bold">Welcome to the Home Page!</h1>
-            </body>
-        </html>
-    );
+    return <h1 className="text-4xl font-bold">Welcome to the Home Page!</h1>
 };
